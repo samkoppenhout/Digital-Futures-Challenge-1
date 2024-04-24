c2d4c7de406da3627d326e8f2c1a0de500bedef2
Sam K <samkoppenhout@live.com>
2024-04-24 18:01:44 +0100

    Updating index with demo of the stormy weather

--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ import airport from "../src/airport.js";
 
 let plane1 = {Name: "Plane 1"}, plane2 = {Name: "Plane 2"}, plane3 = {Name: "Plane 3"}, plane4 = {Name: "Plane 4"}, plane5 = {Name: "Plane 5"}
 
-console.log("In this demonstration there are 5 planes, named Plane 1 to Plane 5.")
+console.log(chalk.blue("In this demonstration there are 5 planes, named Plane 1 to Plane 5."));
 console.log("-----------");
 
-console.log("Setting plane capacity to 2.")
+console.log("Setting plane capacity to 2.");
 airport.setCapacity(2);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
 console.log("-----------");
 
-console.log("Adding plane 1 to airport.")
+console.log("Adding plane 1 to airport.");
 airport.addPlane(plane1);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
 console.log("-----------");
 
-console.log("Adding plane 2 to airport.")
+console.log("Adding plane 2 to airport.");
 airport.addPlane(plane2);
 console.log("-----------");
 
@@ -31,65 +31,76 @@ console.log("Displaying current contents of airport:")
 airport.planeList.forEach(plane => { console.log(plane.Name) });
 console.log("-----------");
 
-console.log("Adding plane 3 to airport.")
+console.log("Adding plane 3 to airport.");
 airport.addPlane(plane3);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
 console.log(chalk.red("This would put the airport over capacity so the plane was not added!"))
 console.log("-----------");
 
-console.log("Removing plane 2 from airport.")
+console.log("Removing plane 2 from airport.");
 airport.removePlane(plane2);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
 console.log("-----------");
 
-console.log("Adding plane 3 to airport.")
+console.log("Adding plane 3 to airport.");
 airport.addPlane(plane3);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
-console.log(chalk.green("Now that the airport is below capacity, plane 3 can be added!"))
+console.log(chalk.green("Now that the airport is below capacity, plane 3 can be added!"));
 console.log("-----------");
 
-console.log("Setting plane capacity to 8.")
+console.log(chalk.blue("Setting plane capacity to 8."));
 airport.setCapacity(8);
 console.log("-----------");
 
-console.log("Adding plane 4 to airport.")
+console.log("Adding plane 4 to airport.");
 airport.addPlane(plane4);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
 console.log(chalk.green("Now that the airport capacity is higher, another plane can be added!"))
 console.log("-----------");
 
-console.log("Removing plane 3 from airport.")
+console.log(chalk.blue("Setting the weather to stormy"));
+airport.isStormy = true;
+console.log("-----------");
+
+console.log("Removing plane 3 from airport.");
 airport.removePlane(plane3);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log(chalk.red("As the weather is stormy, the plane was not removed!"));
 console.log("-----------");
 
-console.log("Removing plane 1 from airport.")
-airport.removePlane(plane1);
+console.log("Adding plane 2 to airport.");
+airport.removePlane(plane2);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log(chalk.red("As the weather is stormy, the plane was not added!"));
 console.log("-----------");
 
-console.log("Adding plane 5 to airport.")
+console.log(chalk.blue("Setting the weather back to not-stormy"));
+airport.isStormy = false;
+console.log("-----------");
+
+console.log("Adding plane 5 to airport.");
 airport.addPlane(plane5);
 console.log("-----------");
 
-console.log("Displaying current contents of airport:")
+console.log("Displaying current contents of airport:");
 airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log(chalk.green("Now that the weather is not stormy, another plane can be added!"));
 console.log("-----------");
