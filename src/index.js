e8a144e35253e1d3d2d99e0f69270cd1461da6a1
Sam K <samkoppenhout@live.com>
2024-04-24 17:50:22 +0100

    Updating index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,95 @@
-import airport from "../src/airport.js"
+import chalk from 'chalk';
 
-let plane1 = {ID: 1}, plane2 = {ID: 2}, plane3 = {ID: 3}, plane4 = {ID: 4}, plane5 = {ID: 5}
+import airport from "../src/airport.js";
 
+let plane1 = {Name: "Plane 1"}, plane2 = {Name: "Plane 2"}, plane3 = {Name: "Plane 3"}, plane4 = {Name: "Plane 4"}, plane5 = {Name: "Plane 5"}
+
+console.log("In this demonstration there are 5 planes, named Plane 1 to Plane 5.")
+console.log("-----------");
+
+console.log("Setting plane capacity to 2.")
 airport.setCapacity(2);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log("-----------");
+
+console.log("Adding plane 1 to airport.")
 airport.addPlane(plane1);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log("-----------");
+
+console.log("Adding plane 2 to airport.")
 airport.addPlane(plane2);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log("-----------");
+
+console.log("Adding plane 3 to airport.")
 airport.addPlane(plane3);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log(chalk.red("This would put the airport over capacity so the plane was not added!"))
+console.log("-----------");
+
+console.log("Removing plane 2 from airport.")
 airport.removePlane(plane2);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log("-----------");
+
+console.log("Adding plane 3 to airport.")
 airport.addPlane(plane3);
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log(chalk.green("Now that the airport is below capacity, plane 3 can be added!"))
+console.log("-----------");
+
+console.log("Setting plane capacity to 8.")
 airport.setCapacity(8);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Adding plane 4 to airport.")
 airport.addPlane(plane4);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log(chalk.green("Now that the airport capacity is higher, another plane can be added!"))
+console.log("-----------");
+
+console.log("Removing plane 3 from airport.")
 airport.removePlane(plane3);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log("-----------");
+
+console.log("Removing plane 1 from airport.")
 airport.removePlane(plane1);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log("-----------");
+
+console.log("Adding plane 5 to airport.")
 airport.addPlane(plane5);
-console.log(airport.planeList)
+console.log("-----------");
+
+console.log("Displaying current contents of airport:")
+airport.planeList.forEach(plane => { console.log(plane.Name) });
+console.log("-----------");
