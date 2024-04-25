0ac359297a4a0e95dfb970c17e0c2ca5cac9f266
Sam K <samkoppenhout@live.com>
2024-04-25 14:33:53 +0100

    Adding error messages to production code

--- src/airport.js
+++ src/airport.js
@@ -9,10 +9,8 @@ const airport = {
 
     // Methods
     setCapacity(newCapacity) {
-        if (newCapacity >= this.planeList.length) {
-            Number.isInteger(newCapacity) && (newCapacity >= 0) ? this.capacity = newCapacity : console.log(`WARNING: The new capacity must be a non-negative integer. Capacity has not been changed.`);
-        }
-    },
+        (newCapacity >= this.planeList.length) && Number.isInteger(newCapacity) && (newCapacity >= 0) ? this.capacity = newCapacity : console.log(`WARNING: Capacity has not been changed. The new capacity must be a non-negative integer, greater than or equal to the number of planes currently at the airport.`);
+        },
 
     isFull() {
         return (this.planeList.length >= this.capacity)
@@ -23,11 +21,11 @@ const airport = {
     },
 
     addPlane(plane) {
-        !(plane === null) && !(this.isFull()) && !(this.searchForPlane(plane)) && !this.isStormy && airport.planeList.push(plane);
+        !(plane === null) && !(this.isFull()) && !(this.searchForPlane(plane)) && !this.isStormy ? airport.planeList.push(plane) : console.log(`WARNING: The plane was not added. The airport must not be full, the plane must not already be at the airport and the weather must not be stormy.`);
     },
 
     removePlane(plane) {
-        !this.isStormy && (this.planeList = this.planeList.filter((query) => !(query === plane)));
+        !this.isStormy && this.planeList.includes(plane) ? (this.planeList = this.planeList.filter((query) => !(query === plane))) : console.log(`WARNING: The plane was not added. The airport must contain the plane already and the weather must not be stormy.`);
     }
 }
 
